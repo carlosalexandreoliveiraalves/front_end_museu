12f599f2afa8e020b5627c8802f41eed8c89b40e
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2024-11-29 20:10:58 -0300

    Feat: Adiciona função para abrir uma janela de confirmação no cadastro

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,6 +10,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
 
+export class SignupComponent  {
+
+  // Injeção do serviço Router
+  constructor(private router: Router) {}
+
+  confirmSignup(): void {
+    console.log("Chamando função para abrir popup...\n");
+    
+    let txt: string;
+
+    if (confirm("Todas as informações estão corretas?")) {
+      txt = "Cadastro confirmado!";
+      
+      // Navegar para a tela inicial após confirmar
+      this.router.navigate(['']);  // Aqui você usa o caminho da tela inicial (geralmente '/')
+      
+    } else {
+      txt = "Você recusou o cadastro!";
+    }
+
+    // Opcional: exibir o texto gerado no console ou em algum outro lugar
+    console.log(txt);
+  }
 }
+
